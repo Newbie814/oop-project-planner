2cca60f4aefd3cd42f8490717cc6cdda40dd0aae
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-07 16:54:29 -0400

    added functionality and foundation for more info pop up

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-class ToolTip {}
-
 class DOMHelper {
-static clearEventListeners(element) {
-  const clonedElement = element.cloneNode(true);
- element.replaceWith(clonedElement);
- return clonedElement;
-}
-
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
 
   static moveElement(elementId, newDestinationSelector) {
     const element = document.getElementById(elementId);
@@ -15,7 +12,34 @@ static clearEventListeners(element) {
   }
 }
 
+class ToolTip {
+  constructor(closeNotifierFunction) {
+    this.closeNotifier = closeNotifierFunction;
+  }
+
+  // arrow function 'this' is automatically the surrounding this not the this of it's own function object
+  closeToolTip = () => {
+    this.destroy();
+    this.closeNotifier();
+  };
+
+  destroy() {
+    this.element.remove();
+  }
+
+  show() {
+    const toolTipElement = document.createElement('div');
+    toolTipElement.className = 'card';
+    toolTipElement.textContent = 'I am a tooltip';
+    toolTipElement.addEventListener('click', this.closeToolTip);
+    this.element = toolTipElement;
+    document.body.append(toolTipElement);
+  }
+}
+
 class ProjectItem {
+  hasActiveToolTip = false;
+
   constructor(id, updateProjectListsFunction, type) {
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunction;
@@ -23,19 +47,39 @@ class ProjectItem {
     this.connectSwitchButton(type);
   }
 
-  connectMoreInfoBtn() {}
+  showMoreInfoHandler() {
+    if (this.hasActiveToolTip) {
+      return;
+    }
+    const toolTip = new ToolTip(() => {
+      this.hasActiveToolTip = false;
+    });
+    toolTip.show();
+    this.hasActiveToolTip = true;
+  }
+
+  connectMoreInfoBtn() {
+    const projectItemElement = document.getElementById(this.id);
+    const moreInfoBtn = projectItemElement.querySelector(
+      'button:first-of-type'
+    );
+    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+  }
 
   connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
     let switchBtn = projectItemElement.querySelector('button:last-of-type');
-   switchBtn = DOMHelper.clearEventListeners(switchBtn);
-   switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
-    switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id));
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+    switchBtn.addEventListener(
+      'click',
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
   }
 
   update(updateProjectListsFunction, type) {
     this.updateProjectListsHandler = updateProjectListsFunction;
-    this.connectSwitchButton(type)
+    this.connectSwitchButton(type);
   }
 }
 
@@ -48,7 +92,11 @@ class ProjectList {
     const projectItems = document.querySelectorAll(`#${type}-projects li`);
     for (const projectItem of projectItems) {
       this.projects.push(
-        new ProjectItem(projectItem.id, this.switchProject.bind(this), this.type)
+        new ProjectItem(
+          projectItem.id,
+          this.switchProject.bind(this),
+          this.type
+        )
       );
     }
     console.log(this.projects);
@@ -59,15 +107,14 @@ class ProjectList {
   }
 
   addProject(project) {
-    this.projects.push(project)
+    this.projects.push(project);
     DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
     project.update(this.switchProject.bind(this), this.type);
   }
 
   switchProject(projectId) {
-    
-this.switchHandler(this.projects.find(p => p.id === projectId))
-this.projects = this.projects.filter(p => p.id !== projectId)
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
