38a39bcae1a8aea95fe5c360fe5394cb3581c9b2
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-14 23:02:00 -0400

    split into components

---
+++ assets/scripts/App/ProjectItems.js
@@ -0,0 +1,69 @@
+import { ToolTip } from './Tooltip.js';
+import { DOMHelper } from '../Utility/DOMhelper.js';
+
+export class ProjectItem {
+  hasActiveToolTip = false;
+
+  constructor(id, updateProjectListsFunction, type) {
+    this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoBtn();
+    this.connectSwitchButton(type);
+    this.connectDragHandler();
+  }
+
+  showMoreInfoHandler() {
+    if (this.hasActiveToolTip) {
+      return;
+    }
+    const projectElement = document.getElementById(this.id);
+    const toolTipText = projectElement.dataset.extraInfo;
+
+    const toolTip = new ToolTip(
+      () => {
+        this.hasActiveToolTip = false;
+      },
+      toolTipText,
+      this.id
+    );
+    toolTip.show();
+    this.hasActiveToolTip = true;
+  }
+
+  connectDragHandler() {
+    const item = document.getElementById(this.id);
+    item.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', this.id);
+      event.dataTransfer.effectAllowed = 'move';
+    });
+    console.log(item);
+
+    item.addEventListener('dragend', (event) => {
+      console.log(event);
+    });
+  }
+
+  connectMoreInfoBtn() {
+    const projectItemElement = document.getElementById(this.id);
+    const moreInfoBtn = projectItemElement.querySelector(
+      'button:first-of-type'
+    );
+    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler.bind(this));
+  }
+
+  connectSwitchButton(type) {
+    const projectItemElement = document.getElementById(this.id);
+    let switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+    switchBtn.addEventListener(
+      'click',
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
+  }
+
+  update(updateProjectListsFunction, type) {
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectSwitchButton(type);
+  }
+}
